0b8b527f2bb86e5cdb9fbe7ff2cf0fb69ef4f15d
dinhhuy <93317911+dinhhuy2001@users.noreply.github.com>
2022-08-03 22:29:22 +0700

    commit

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const dropDown = document.querySelector(".drop-down-menu");
 const openMenu = document.querySelector(".menu-drop");
 
+
 function toggleDrop() {
   dropDown.classList.toggle("hide");
 }
 
 openMenu.addEventListener("click", toggleDrop);
+
+
